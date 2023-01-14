e0e692c02bfac63ffb38b39b625d335dac66fa09
blakemckeany <blakemckeany@gmail.com>
2023-01-14 18:16:41 -0500

    Added test cases and function for reading a customer account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -134,11 +134,7 @@ class TestAccountService(TestCase):
         data = response.get_json()
         self.assertEqual(data["name"], account.name)
 
-    def test_read_an_account_missing(self):
-        """ It should fail to find a single account """
-        account = self._create_accounts(1)[0]
-        response = self.client.get(
-            f"{BASE_URL}/13123", content_type="application/json"
-        )
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        data = response.get_json()
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
